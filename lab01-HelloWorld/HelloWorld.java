f066c2f0a5d85645c42493b13c8106d0abcfa6fb
jrd419 <jrd419@lehigh.edu>
2015-08-27 15:41:47 +0000

    updatedHelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+
+//Jacob Dooleys
+//Lab 1
+//Hello World Java Program
+//
+//  javac HelloWorld.java
+//  java HelloWorld//
+
+public class HelloWorld{
+    
+    public static void main(String[] args) {
+        
+        System.out.println("#####################");
+        System.out.println("#####################");
+        System.out.println("#                   #");
+        System.out.println("# Hello World 2015! #");
+        System.out.println("#                   #");
+        System.out.println("#####################");
+        System.out.println("#####################");
+        System.out.println("test");
+        
+        //test/testtest
+    }
+}
